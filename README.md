73d72f6ad22bfdadb06988d942c6ddd8aa1895be
Alexander <apeter@gmx.de>
2018-03-10 14:01:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # updhtaccess
-Update dynamic IPv6 address ranges in .htaccess files on Apache 2.4
+bash script for Synology DiskStation task-planner.
+
+This script determines the IPv6 address of the Diskstation and puts the prefix (/64) to the list of granted useres who do not need to enter a password for a restriced area.
+
+The script expects a valid .htaccess.tmpl file (also the .htpasswd as used in the sample....).
+
+If a template is available it will be copied to .htaccess and amended by the local IPv6 Network (the local LAN...) and a requirement for a valid user.
+
+Put this script in your admin home of you DSM and add a task with the rights of the user admin.
+
+In my case I update once a day as my IPv6 Provider prefix changes once a day.
---
+++ updhtaccess.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Dynamic IPv6 .htaccess file generator
+#
+# Usage: ./updhtaccess.sh <directory>
+# <directory> without trailing slash!
+# example: ./updhtaccess.sh /volume1/web/private
+
+hta_folder="$1"
+hta_file="$1/.htaccess"
+hta_tmpl="$hta_file.tmpl"
+
+#check if template available
+if [ -f "$hta_tmpl" ]
+then
+	echo "$hta_tmpl found."
+else
+	echo "$hta_tmpl not found. updhtaccess.sh terminated!"
+	exit
+fi
+
+# DSM implementation
+hta_ipv6=$(ifconfig eth0 | grep Scope:Global | awk '/inet6/{print $3}')
+# macOS for testing
+#hta_ipv6=$(ifconfig en0 | grep 'autoconf secured' | awk '/inet6/{print $2}')
+
+hta_prefix="Require ip $hta_ipv6"
+
+# create new .htaccess file based on template
+cp $hta_tmpl $hta_file
+echo "$hta_prefix" >> $hta_file 2>&1
+echo "Require valid-user" >> $hta_file 2>&1
+echo "$hta_file written."
